c9f33a91386639b5608389be7c7972b42ad8eadb
jackmalboeuf <jmalboeuf70@gmail.com>
2020-07-13 11:13:45 -0500

    Player Movement
    Implemented player movement and input management for two players.

---
+++ CookingMaster/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Speed at which the player moves
+    public float movementSpeed = 5;
+
+    //Input axes
+    public string horizontalInput;
+    public string verticalInput;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //Horizontal movement
+        if (Input.GetAxis(horizontalInput) != 0)
+        {
+            rb.velocity = new Vector2(Input.GetAxis(horizontalInput) * movementSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);    //if the player isn't pressing any buttons then stop moving
+        }
+
+        //Vertical movement
+        if (Input.GetAxis(verticalInput) != 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Input.GetAxis(verticalInput) * movementSpeed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);    //if the player isn't pressing any buttons then stop moving
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CookingMasterProgrammingTest
  Programming test for Tentworks
+
+
+Implementation
+
+Player Movement
+
+1. Enter desired movement speed. 
+2. Setup input axes for desired number of players
+	a. Create a horizontal and vertical axis in the input manager labeled for each player (P1, P2, etc.)
+	b. Enter positive and negative buttons for each player (wasd, arrow keys) excluding alt buttons
+	c. Set Gravity and Sensitivity for each axis to at least 50 and keep Dead at 0.001
+3. Enter the names of the corresponding axes into the "Horizontal Input" and "Vertical Input" fields for each player
+
